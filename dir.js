c4d399ce464b17be2b4869991095ec9fc9295ca3
Mike <korostelevm@gmail.com>
2023-04-05 10:56:29 -0400

    split

--- dir.js
+++ dir.js
@@ -1,6 +1,23 @@
 const dirTree = require("directory-tree");
-const tree = dirTree(".",{attributes: ["size", "type"]});
+let callback = (item, PATH, stats, c) => {
+    item.size_disk = stats.blocks * 512
+    if(item.type == 'directory'){
+        item.size_disk = item.children.map(child => child.size_disk).reduce((accumulator, currentValue) => {
+            return accumulator + currentValue;
+          }, 0);
+  }
+}
+// const tree = dirTree("./dir",{
+const tree = dirTree("/tmp/korostelevm-yo-3/",{
+    attributes: ["type"],
+    exclude: [/\.git/, /\.npm/, /\.cache/] 
+},callback,callback);
+
+let size_mb = tree.size_disk / 1000000
+size_mb = Math.round(size_mb * 100) / 100
+// const tree = dirTree("./dir",{attributes: ["type"]},callback,callback);
 const fs = require('fs')
 fs.writeFileSync('dirs.json', JSON.stringify(tree))
 console.log(tree)
+console.log(size_mb)
 
